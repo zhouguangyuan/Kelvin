ab4f89108ad80433b2b1546b7ea7dad803624369
zhouguangyuan <kevinsoft@126.com>
2020-09-27 09:44:07 +0800

    Complet the Person class

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,29 @@
-
 public class Person {
+	private String name = null;
+	private int age = 0;
 
+	public Person() {
+
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
